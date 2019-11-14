38e965cebda59951db76a7e0d4c8a82fcaf0fbdf
Apiyo4 <odingoannette@gmail.com>
2019-11-14 17:59:39 +0300

    Create a form with Formik

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import {withFormik, Form,Field} from 'formik';
 
-function UserForm(){
+function UserForm(props){
+    console.log(props);
     return(
-        <h1> Trial</h1>
+        <form>
+            <label>Name:
+                <Field type="text" name="name" placeholder="Enter name" />
+            </label>
+            <br />
+            <label>Email:
+                <Field type='text' name='email' placeholder='Enter email' />
+            </label>
+            <br />
+            <label> Password:
+                <Field type='password' name='password' placeholder='Enter password' />
+            </label>
+            <br />
+            <label> Term of Service
+                <Field type='checkbox' name="term_of_service" />
+            </label>
+            <br />
+            <br />
+            <input type="submit" />
+
+        </form>
     )
 }
-export default UserForm;
+
+
+const UserFormWithFormik = withFormik({
+    mapPropsToValues(){
+        return{
+            name: '',
+            email: '',
+            password: '',
+            term_of_service: false
+        }
+    }
+})(UserForm);
+export default UserFormWithFormik;
